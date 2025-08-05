3e092299a5de17beac1c41e7203dfc3332348f6b
Parsa Rahimi <mail@parsuli.net>
2025-08-04 18:19:08 -0700

    Add prod base URL to config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import path from 'node:path';
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-		},
-	},
+  plugins: [react(), tailwindcss()],
+  base: "https://par5ul1.github.io/sprout/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
